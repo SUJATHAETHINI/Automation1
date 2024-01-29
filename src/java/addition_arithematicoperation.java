4894c42e775bcb63620a184c6e39dfaf1a1587f9
ethin <ethin@192.168.100.21>
2024-01-29 12:40:45 +0530

    its my second program on github

---
+++ src/java/addition_arithematicoperation.java
@@ -0,0 +1,13 @@
+package java;
+
+public class addition_arithematicoperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aa=50;int bb=100;int cc;
+		cc=aa+bb;
+		System.out.println(cc);
+
+	}
+
+}
